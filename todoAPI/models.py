8a4189f54b7f40618e827389eabf9116c99bb82a
KarimovProject <abdullatifkarimov772@gmail.com>
2022-11-08 17:59:36 +0500

    app and project created

---
+++ todoAPI/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    task = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task

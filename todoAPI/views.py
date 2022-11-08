6f3682123be24ce9caeafcd093cecc9c9c15bf68
KarimovProject <abdullatifkarimov772@gmail.com>
2022-11-08 18:27:44 +0500

    views is ready

--- todoAPI/views.py
+++ todoAPI/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views import View
+from .models import Task
 # Create your views here.
+class Get_all(View):
+    def get(self, request):
+        todo_all = Task.objects.all()
+        todo_all_json = {'results':[]}
+        for i in todo_all:
+            todo_all_json['results'].append(i.todo_json())
+        return JsonResponse(todo_all_json)

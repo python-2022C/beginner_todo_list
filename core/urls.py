36b59f6ff688340db7b010c35fd73fcc64f4efdd
DaminovSherzod <daminovsherzod2002@gmail.com>
2022-11-10 16:36:45 +0500

    get task

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todoAPI.views import GetTask
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get/<int:id>/', GetTask.as_view())
 ]
